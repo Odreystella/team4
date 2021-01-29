848c876af6c004a9a93a9712fdf8eed146bcbce7
ssungs <asdf4651@naver.com>
2021-01-29 17:13:16 +0900

    feat: Add level, chance
    Add level,chance
    
    print life(s)
    
    solved: #1

--- numguess.py
+++ numguess.py
@@ -2,21 +2,23 @@ import random
 
 
 answer = random.randint(1, 100)
-chance = 3
+chance = int(input('Choose level(10-easy 1-hell): '))
 
 while chance > 0:
 
-    guess - int(input('Guess the number(1~100): '))
+    guess = int(input('Guess the number(1~100): '))
     if guess-answer==0:
-        print('Correct!)
+        print('Correct!')
         break
-    elif guess<answer:
+    elif guess < answer:
         hint = 'up!'
-    elif guess>answer:
+    elif guess > answer:
         hint = 'Down!'
-    chance -=1
-    print('{}' You have {} life(s).format(hint, chance))
+    chance -= 1
+    print('{} You have {} life(s)'.format(hint, chance))
 
-    
+    if chance == 0:
+        print('YOu now dead! The answer was {}.'.format(answer))
+
 
 
